1495a9d66606b31978957777f814ab132c2e50b9
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2025-03-19 20:43:49 -0700

    Consuming REST API

---
+++ src/main/java/com/example/consuming_rest/Quote.java
@@ -0,0 +1,9 @@
+/*
+* Domain class to contain the data that you need
+* */
+package com.example.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Quote(String type, Value value) { }
